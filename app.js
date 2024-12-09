80dc9df93996dcb3c237a63a19be0d1f59e1acf4
haseebkhan9081 <aa9081haseeb@gmail.com>
2024-12-09 14:05:18 +0500

    increased file size for multer uplods

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ import mongoose from 'mongoose';
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 app.use(morgan('dev'));
 
